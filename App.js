678128cacf05dcb79c06d4d60ffb01e7f6d5048a
fayzanhun <147543521+fayzanhun@users.noreply.github.com>
2024-01-08 22:18:10 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,41 @@
+import { React } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./Home";
+import About from "./About";
+import Product from './Product';
+import Contact from './Contact';
+import SingleProduct from "./SingleProduct";
+import Cart from './Cart';
+import ErrorPage from './ErrorPage';
+import { GlobalStyle } from './GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+
+const App = () => {
+
+const theme = {
+  colors: {
+    bg: "#fff",
+  },
+};
+  return (
+    <ThemeProvider theme={theme}>
+    <tab>    
+    <Router>
+      <GlobalStyle />
+  <Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/about" element={<About />} />
+  <Route path="/product" element={<Product />} />
+  <Route path="/contact" element={<Contact />} />
+  <Route path="/singleproduct/:id" element={<SingleProduct />} />
+  <Route path="/cart" element={<Cart />} /> 
+  <Route path="*" element={<ErrorPage />} /> 
+</Routes>
+  </Router>
+  </tab>
+  </ThemeProvider>
+
+  );
+};
+
+export default App;
